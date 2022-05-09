bf0d71bb78e6990c76bf94ff4d8a438a60863406
Matt Eilar <meilar@gmail.com>
2022-05-09 09:55:26 -0700

    Complete headline component

--- src/Headlines.js
+++ src/Headlines.js
@@ -1,6 +1,29 @@
 import React from "react";  
 
 class Headlines extends React.Component {
+
+  makeApiCall = () => {
+    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+      .then(response => response.json())
+      .then(
+        (jsonifiedResponse) => {
+          this.setState({
+            isLoaded: true,
+            headlines: jsonifiedResponse
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isLoaded:true,
+            error
+          });
+        });
+  }
+
+  componentDidMount() {
+    this.makeApiCall()
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -11,7 +34,25 @@ class Headlines extends React.Component {
   }
 
   render() {
-
+    const { error, isLoaded, headlines } = this.state;
+    if (error) {
+      return <React.Fragment>Error: {error.message}</React.Fragment>
+    } else if (!isLoaded) {
+      return <React.Fragment>Loading...</React.Fragment>
+    } else {
+      return (
+        (<React.Fragment>
+          <h1>Headlines</h1>
+          <ul>
+            {headlines.map((headline, index) =>
+            <li key={index}>
+              <h3>{headline.title}</h3>
+              <p>{headline.abstract}</p>
+            </li>)}
+          </ul>
+        </React.Fragment>)
+      );
+    }
   }
 }
 

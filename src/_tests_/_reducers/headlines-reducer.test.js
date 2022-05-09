084b2c4da5ccbeade42e8a210759847f81923abb
Matt Eilar <meilar@gmail.com>
2022-05-09 12:50:28 -0700

    Improve headline reducer

--- src/_tests_/_reducers/headlines-reducer.test.js
+++ src/_tests_/_reducers/headlines-reducer.test.js
@@ -1,4 +1,5 @@
 import headlinesReducer from '../../reducers/headlines-reducer';
+import * as c from './../../actions/ActionTypes'
 
 describe('headlinesReducer', () => {
   const defaultState = {
@@ -6,6 +7,12 @@ describe('headlinesReducer', () => {
     headlines: [],
     error: null
   };
+
+  const loadingState = {
+    isLoading: true,
+    headlines: [],
+    error: null
+  }
 
   test('should sxs return default state if no action is passed', () => {
     expect(headlinesReducer(defaultState, {type: null})).toEqual(
@@ -15,5 +22,29 @@ describe('headlinesReducer', () => {
         error: null
       }
     );
+  });
+
+  test('requesting headlines should change isLoading from false to true', () => {
+    const action = { type: c.REQUEST_HEADLINES };
+
+    expect(headlinesReducer(defaultState, action)).toEqual({
+      isLoading: true,
+      headlines: [],
+      error: null
+    });
+  });
+
+  test('successfully getting headlines should change isLoading to false and update headlines', ()=> {
+    const headlines = "A headline";
+    const action = {
+      type: c.GET_HEADLINES_SUCCESS,
+      headlines: headlines
+    };
+
+    expect(headlinesReducer(loadingState, action)).toEqual({
+      isLoading: false,
+      headlines: "A headline",
+      error: null
+    });
   });
 });
